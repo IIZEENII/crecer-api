c2a06b894dbaa3382332cc1dd4349754ae261d75
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-24 17:16:54 -0600

    feat: add employee controller

--- src/employees/infrastructure/employees.controller.ts
+++ src/employees/infrastructure/employees.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
+import { CreateEmployeeDto } from './dtos/create-employee.dto';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+
+  @Post()
+  async create(@Body() createEmployeeDto: CreateEmployeeDto): Promise<void> {
+    this.employeesService.create(createEmployeeDto);
+  }
 }
