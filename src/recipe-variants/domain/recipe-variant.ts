3efd51ecd942c50b578894d4177545ee3e4bd7d0
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-25 15:25:56 -0600

    feat: rename realtion table and add new relations

--- src/recipe-variants/domain/recipe-variant.ts
+++ src/recipe-variants/domain/recipe-variant.ts
@@ -1,21 +1,27 @@
+import { Ingredient } from 'src/ingredients/domain/ingredient';
 import { Procedure } from 'src/procedures/domain/procedure';
 import { Product } from 'src/products/domain/product';
 import { Recipe } from 'src/recipes/domain/recipe';
 import {
   Column,
   Entity,
+  JoinTable,
+  ManyToMany,
   ManyToOne,
   OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
-@Entity('recipeVariants')
+@Entity('recipe_variants')
 export class RecipeVariant {
   @PrimaryGeneratedColumn('uuid')
   id: string;
   @Column()
   name: string;
+  @ManyToMany(() => Ingredient)
+  @JoinTable({ name: 'recipe_variants_and_ingredients' })
+  ingredients: Ingredient[];
   @OneToMany(() => Procedure, (procedure) => procedure.recipeVariant)
   procedures: Procedure[];
   @ManyToOne(() => Recipe, (recipe) => recipe.variants)
