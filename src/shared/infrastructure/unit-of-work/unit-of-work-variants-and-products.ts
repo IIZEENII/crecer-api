07e8fe5e652a0c49983dc9aafd4d9ce078791eb4
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-25 01:47:07 -0600

    feat: update variants and products unit of work implementation

--- src/shared/infrastructure/unit-of-work/unit-of-work-variants-and-products.ts
+++ src/shared/infrastructure/unit-of-work/unit-of-work-variants-and-products.ts
@@ -1,34 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { InjectEntityManager } from '@nestjs/typeorm';
-import { EntityManager, Repository } from 'typeorm';
+import { EntityManager, QueryRunner, Repository } from 'typeorm';
 import { UnitOfWork } from './unit-of-work';
 import { RecipeVariant } from 'src/recipe-variants/domain/recipe-variant';
 import { Product } from 'src/products/domain/product';
 
 @Injectable()
 export class UnitOfWorkVariantsAndProducts implements UnitOfWork {
+  private queryRunner: QueryRunner;
+
   constructor(
     @InjectEntityManager()
     private readonly entityManager: EntityManager,
   ) {}
 
   async beginTransaction(): Promise<void> {
-    this.entityManager.queryRunner.startTransaction();
+    this.queryRunner = this.entityManager.connection.createQueryRunner();
+    this.queryRunner.startTransaction();
   }
 
   async commitTransaction(): Promise<void> {
-    this.entityManager.queryRunner.commitTransaction();
+    if (this.queryRunner) {
+      await this.queryRunner.commitTransaction();
+      await this.releaseQueryRunner();
+    }
   }
 
   async rollbackTransaction(): Promise<void> {
-    this.entityManager.queryRunner.rollbackTransaction();
+    if (this.queryRunner) {
+      await this.queryRunner.rollbackTransaction();
+      await this.releaseQueryRunner();
+    }
+  }
+
+  private async releaseQueryRunner(): Promise<void> {
+    if (this.queryRunner) {
+      await this.queryRunner.release();
+      this.queryRunner = null;
+    }
   }
 
   get recipeVariantRepository(): Repository<RecipeVariant> {
-    return this.entityManager.getRepository(RecipeVariant);
+    return this.queryRunner.manager.getRepository(RecipeVariant);
   }
 
   get productRepository(): Repository<Product> {
-    return this.entityManager.getRepository(Product);
+    return this.queryRunner.manager.getRepository(Product);
   }
 }
