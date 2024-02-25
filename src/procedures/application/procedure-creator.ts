a80d15e98f3fce5d3c51974078a544c3b49b3e41
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-24 23:13:13 -0600

    feat: add procedure creator

---
+++ src/procedures/application/procedure-creator.ts
@@ -0,0 +1,17 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Procedure } from '../domain/procedure';
+import { Repository } from 'typeorm';
+import { CreateProcedureDto } from '../infrastructure/dtos/create-procedure.dto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProcedureCreator {
+  constructor(
+    @InjectRepository(Procedure)
+    private readonly procedureRepository: Repository<Procedure>,
+  ) {}
+
+  async create(createProcedureDto: CreateProcedureDto): Promise<void> {
+    this.procedureRepository.create(createProcedureDto);
+  }
+}
