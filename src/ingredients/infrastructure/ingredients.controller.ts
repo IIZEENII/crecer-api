1f0852d132670a371a78ee2d83547b34b34e2704
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-25 16:54:01 -0600

    feat: add create method in controller

--- src/ingredients/infrastructure/ingredients.controller.ts
+++ src/ingredients/infrastructure/ingredients.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
+import { CreateIngredientDto } from './dtos/create-ingredient.dto';
 
 @Controller('ingredients')
 export class IngredientsController {
   constructor(private readonly ingredientsService: IngredientsService) {}
+
+  @Post()
+  async create(
+    @Body() createIngredientDto: CreateIngredientDto,
+  ): Promise<void> {
+    this.ingredientsService.create(createIngredientDto);
+  }
 }
