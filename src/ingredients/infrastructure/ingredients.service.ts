543231ae0ec5ce88bfb895b8d39b6fb467eaf7a2
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-25 16:53:09 -0600

    feat: add create method in service

--- src/ingredients/infrastructure/ingredients.service.ts
+++ src/ingredients/infrastructure/ingredients.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { IngredientCreator } from '../application/ingredient-creator';
+import { CreateIngredientDto } from './dtos/create-ingredient.dto';
 
 @Injectable()
-export class IngredientsService {}
+export class IngredientsService {
+  constructor(private readonly ingredientCreator: IngredientCreator) {}
+
+  async create(createIngredientDto: CreateIngredientDto): Promise<void> {
+    this.ingredientCreator.create(createIngredientDto);
+  }
+}
