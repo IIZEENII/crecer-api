e06c9a7bdc1a31433d2253a1ab25ecd279d89067
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-25 01:49:15 -0600

    feat: update recipe variant entity

--- src/recipe-variants/domain/recipe-variant.ts
+++ src/recipe-variants/domain/recipe-variant.ts
@@ -1,10 +1,12 @@
 import { Procedure } from 'src/procedures/domain/procedure';
+import { Product } from 'src/products/domain/product';
 import { Recipe } from 'src/recipes/domain/recipe';
 import {
   Column,
   Entity,
   ManyToOne,
   OneToMany,
+  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -18,4 +20,6 @@ export class RecipeVariant {
   procedures: Procedure[];
   @ManyToOne(() => Recipe, (recipe) => recipe.variants)
   recipe: Recipe;
+  @OneToOne(() => Product, (product) => product.recipeVariant)
+  product: Product;
 }
