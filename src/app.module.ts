8f32b8f05bd5498f9403ef7a2a91349cd39c83ea
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-24 22:26:37 -0600

    feat: add recipe variant and procedure entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,10 @@ import { RecipesModule } from './recipes/infrastructure/recipes.module';
 import { Recipe } from './recipes/domain/recipe';
 import { EmployeesModule } from './employees/infrastructure/employees.module';
 import { Employee } from './employees/domain/employee';
+import { RecipeVariantsModule } from './recipe-variants/infrastructure/recipe-variants.module';
+import { RecipeVariant } from './recipe-variants/domain/recipe-variant';
+import { ProceduresModule } from './procedures/infrastructure/procedures.module';
+import { Procedure } from './procedures/domain/procedure';
 
 @Module({
   imports: [
@@ -16,11 +20,13 @@ import { Employee } from './employees/domain/employee';
       username: 'postgres',
       password: 'Utmisjimenez21032002',
       database: 'crecer-database',
-      entities: [Recipe, Employee],
+      entities: [Recipe, RecipeVariant, Procedure, Employee],
       synchronize: true,
     }),
     RecipesModule,
     EmployeesModule,
+    RecipeVariantsModule,
+    ProceduresModule,
   ],
   controllers: [AppController],
   providers: [AppService],
