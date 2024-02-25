a30e894b1e5f9d47c4d7c9e515e0e83e977a1cf3
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-25 15:07:12 -0600

    feat: replace privacity string and array value to enum

--- src/recipes/domain/recipe.ts
+++ src/recipes/domain/recipe.ts
@@ -1,5 +1,7 @@
 import { RecipeVariant } from 'src/recipe-variants/domain/recipe-variant';
+import { Categories } from 'src/shared/domain/categories';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Privacity } from './privacity';
 
 @Entity('recipes')
 export class Recipe {
@@ -7,8 +9,12 @@ export class Recipe {
   id: string;
   @Column()
   title: string;
-  @Column({ enum: ['public', 'private'], default: 'private' })
-  privacity: string;
+  @Column()
+  bookCover: string;
+  @Column({ type: 'enum', enum: Privacity, default: Privacity.PRIVATE })
+  privacity: Privacity;
+  @Column({ type: 'enum', enum: Categories })
+  categories: Categories;
   @OneToMany(() => RecipeVariant, (recipeVariant) => recipeVariant.recipe)
   variants: RecipeVariant[];
 }
