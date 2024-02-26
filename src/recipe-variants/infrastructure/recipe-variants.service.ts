27b51680607cff2507514a50471c2ac5a1363c47
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-25 20:37:04 -0600

    feat: add clone variant method to sevice

--- src/recipe-variants/infrastructure/recipe-variants.service.ts
+++ src/recipe-variants/infrastructure/recipe-variants.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { RecipeVariantCreator } from '../application/recipe-variant-creator';
 import { CreateRecipeVariantDto } from './dtos/create-recipe-variant.dto';
+import { RecipeVariantCloner } from '../application/recipe-variant-cloner';
 
 @Injectable()
 export class RecipeVariantsService {
-  constructor(private readonly recipeVariantCreator: RecipeVariantCreator) {}
+  constructor(
+    private readonly recipeVariantCreator: RecipeVariantCreator,
+    private readonly recipeVariantCloner: RecipeVariantCloner,
+  ) {}
 
   async create(createRecipeVariantDto: CreateRecipeVariantDto): Promise<void> {
     this.recipeVariantCreator.create(createRecipeVariantDto);
+  }
+
+  async cloneVariant(idOfTheReferenceToClone: string): Promise<void> {
+    this.recipeVariantCloner.cloneVariant(idOfTheReferenceToClone);
   }
 }
