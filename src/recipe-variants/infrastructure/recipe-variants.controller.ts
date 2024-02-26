2235d360eeae1ea6417f16d71bc74e6364b4dd2d
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-25 20:35:01 -0600

    feat: add copy method to controller

--- src/recipe-variants/infrastructure/recipe-variants.controller.ts
+++ src/recipe-variants/infrastructure/recipe-variants.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { RecipeVariantsService } from './recipe-variants.service';
 import { CreateRecipeVariantDto } from './dtos/create-recipe-variant.dto';
 
@@ -6,10 +6,18 @@ import { CreateRecipeVariantDto } from './dtos/create-recipe-variant.dto';
 export class RecipeVariantsController {
   constructor(private readonly recipeVariantsService: RecipeVariantsService) {}
 
+  // TODO: replace method for copy
   @Post()
   async create(
     @Body() createRecipeVariantDto: CreateRecipeVariantDto,
   ): Promise<void> {
     this.recipeVariantsService.create(createRecipeVariantDto);
+  }
+
+  @Post('/copy/:id')
+  async cloneVariant(
+    @Param('id') idOfTheReferenceToClone: string,
+  ): Promise<void> {
+    this.recipeVariantsService.cloneVariant(idOfTheReferenceToClone);
   }
 }
