2d46e4b8da92f0c47aa775079d5e9e5f14de4a59
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-25 20:32:27 -0600

    fix: replace repository pattern to unit fo work for transactions

--- src/recipes/application/recipe-creator.ts
+++ src/recipes/application/recipe-creator.ts
@@ -1,18 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRecipeDto } from '../infrastructure/dtos/create-recipe.dto';
-import { Repository } from 'typeorm';
-import { Recipe } from '../domain/recipe';
-import { InjectRepository } from '@nestjs/typeorm';
+import { UnitOfWorkForRecipes } from 'src/shared/infrastructure/unit-of-work/unit-of-work-for-recipes';
 
 @Injectable()
 export class RecipeCreator {
-  constructor(
-    @InjectRepository(Recipe)
-    private readonly recipeRepository: Repository<Recipe>,
-  ) {}
+  constructor(private readonly unitOfWork: UnitOfWorkForRecipes) {}
 
   async create(createRecipeDto: CreateRecipeDto): Promise<void> {
-    const recipeCreated = this.recipeRepository.create(createRecipeDto);
-    this.recipeRepository.save(recipeCreated);
+    try {
+      await this.unitOfWork.beginTransaction();
+
+      const recipeCreated =
+        this.unitOfWork.recipeRepository.create(createRecipeDto);
+
+      await this.unitOfWork.recipeRepository.save(recipeCreated);
+
+      const defaultRecipeVariantCreated =
+        this.unitOfWork.recipeVariantRepository.create({
+          name: 'variante_1',
+          recipe: recipeCreated,
+          category: recipeCreated.category,
+        });
+
+      await this.unitOfWork.recipeVariantRepository.save(
+        defaultRecipeVariantCreated,
+      );
+
+      const defaultProductCreated = this.unitOfWork.productRepository.create({
+        name: defaultRecipeVariantCreated.name,
+        category: recipeCreated.category,
+        recipeVariant: defaultRecipeVariantCreated,
+      });
+
+      await this.unitOfWork.productRepository.save(defaultProductCreated);
+      await this.unitOfWork.commitTransaction();
+    } catch (error) {
+      await this.unitOfWork.rollbackTransaction();
+      console.log(error);
+    }
   }
 }
