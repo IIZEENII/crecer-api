d4634510749aa696d8cb0700f7aee35633bfc010
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-26 20:12:00 -0600

    feat: add ingredient finder use case

---
+++ src/ingredients/application/ingredient-finder.ts
@@ -0,0 +1,18 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Ingredient } from '../domain/ingredient';
+import { Repository } from 'typeorm';
+
+export class IngredientFinder {
+  constructor(
+    @InjectRepository(Ingredient)
+    private readonly ingredientRepository: Repository<Ingredient>,
+  ) {}
+
+  async findById(id: string): Promise<Ingredient> {
+    return this.ingredientRepository.findOneBy({ id });
+  }
+
+  async findAll(): Promise<Ingredient[]> {
+    return this.ingredientRepository.find();
+  }
+}
