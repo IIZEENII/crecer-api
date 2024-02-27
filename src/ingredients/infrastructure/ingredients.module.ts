9cfe66e7614744018a6a7e8a537c12bf5aa8a7fb
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-26 20:12:55 -0600

    feat: add ingredient finder to ingredient module

--- src/ingredients/infrastructure/ingredients.module.ts
+++ src/ingredients/infrastructure/ingredients.module.ts
@@ -4,10 +4,11 @@ import { IngredientsController } from './ingredients.controller';
 import { IngredientCreator } from '../application/ingredient-creator';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ingredient } from '../domain/ingredient';
+import { IngredientFinder } from '../application/ingredient-finder';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Ingredient])],
   controllers: [IngredientsController],
-  providers: [IngredientsService, IngredientCreator],
+  providers: [IngredientsService, IngredientCreator, IngredientFinder],
 })
 export class IngredientsModule {}
