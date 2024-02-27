0ed9869482ad164b0642235c7964c6fe2fa5d056
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-02-26 20:15:20 -0600

    feat: implement ingredient finder into service

--- src/ingredients/infrastructure/ingredients.service.ts
+++ src/ingredients/infrastructure/ingredients.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { IngredientCreator } from '../application/ingredient-creator';
 import { CreateIngredientDto } from './dtos/create-ingredient.dto';
+import { IngredientFinder } from '../application/ingredient-finder';
+import { Ingredient } from '../domain/ingredient';
 
 @Injectable()
 export class IngredientsService {
-  constructor(private readonly ingredientCreator: IngredientCreator) {}
+  constructor(
+    private readonly ingredientCreator: IngredientCreator,
+    private readonly ingredientFinder: IngredientFinder, 
+  ) {}
 
   async create(createIngredientDto: CreateIngredientDto): Promise<void> {
     this.ingredientCreator.create(createIngredientDto);
+  }
+
+  async findById(id: string): Promise<Ingredient> {
+    return this.ingredientFinder.findById(id);
+  }
+
+  async findAll(): Promise<Ingredient[]> {
+    return this.ingredientFinder.findAll();
   }
 }
