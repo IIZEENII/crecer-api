dcdba71cc505a64f03d6bcc0e3b7b8f2578930de
Adrian Mis Jimenez <misjimenezadrianalejandro@gmail.com>
2024-03-03 21:08:00 -0600

    feat: active validations on DTOS with class validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
 import { HttpExceptionFilter } from './shared/infrastructure/HttpExceptionFilter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(8080);
 }
